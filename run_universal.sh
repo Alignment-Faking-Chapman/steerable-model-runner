fe5eda347f054039b71bff815d2fcc794f2c2988
srku43384 <srku43384@chapman.edu@bright81.chapman.edu>
2026-06-17 14:04:10 -0700

    fix: allocated more GPUs

--- run_universal.sh
+++ run_universal.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=gpuq
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH --time=1-00:00:00
